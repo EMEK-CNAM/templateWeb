93c7b8e83d4aee71502f29dd8559100ea6482356
mekhinini <elies.mekhinini@etu.unistra.fr>
2025-10-30 15:51:15 +0100

    credentials

--- api/config.js
+++ api/config.js
@@ -1,11 +1,11 @@
-module.exports =  {
-    ACCESS_TOKEN_SECRET : "EMMA123",
-    BDD : {
-    "host" :"dpg-d40798bipnbc73cf4njg-a.oregon-postgres.render.com",
-    "port" : "5432",
-    "user" : "cnam_qnjc_user",
-    "password" : "FOYjmLxOmqwdnfGPhMO4R8eZpErPzVXQ",
-    "bdname" :"cnam_qnjc" 
+module.exports = {
+    ACCESS_TOKEN_SECRET: "EMMA123",
+    BDD: {
+        "host": "dpg-d41mvc7diees73ejod70-a.oregon-postgres.render.com",
+        "port": "5432",
+        "user": "pollution_9owx_user",
+        "password": "y1OktgrW2upUL93Sz9Eivey8K2wrQTH1",
+        "bdname": "pollution_9owx"
     }
 }
 
